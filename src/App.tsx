71881e4c96e641270a018c3729017f57c39025ad
Rasm-P <rasmus.pra@gmail.com>
2023-03-18 23:16:30 +0100

    Added submit page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ShoppingList from './ShoppingList/ShoppingList'
 import './App.css'
 import Navbar from './Navigation/Navbar'
 import Delivery from './Delivery/Delivery'
+import Submit from './Submit/Submit'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 const address: Address = {
@@ -35,6 +36,7 @@ function App() {
                 <Routes>
                     <Route path='/' element= { <ShoppingList items={items} setItems={setItems} /> } />
                     <Route path='/delivery' element={<Delivery billingAddress={billingAddress} setBilling={setBilling} shippingAddress={shippingAddress} setShipping={setShipping} address={address} check={check} setCheck={setCheck}/>}/>
+                    <Route path='/submit' element={<Submit cartItems={items} billingAddress={billingAddress} shippingAddress={shippingAddress} />}/>
                 </Routes>                  
             </div>
         </div>
---
+++ src/Submit/Submit.tsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Order } from "../interfaces/Order"
+import { CartItem } from "../interfaces/CartItem"
+import "./Submit.css";
+import { Address } from "../interfaces/Address";
+
+const submitUrl = "https://eoysx40p399y9yl.m.pipedream.net";
+
+function Submit({cartItems, billingAddress, shippingAddress} : {cartItems: CartItem[], billingAddress: Address, shippingAddress: Address}) {
+    const navigate = useNavigate();
+    const [marketing, setMarketing] = useState(false);
+    const [comment, setComment] = useState("");
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const order: Order = {
+            cartItems: cartItems,
+            billingAddress: billingAddress,
+            shippingAddress: shippingAddress,
+            checkMarketing: marketing,
+            submitComment: comment
+        }
+        const headers = new Headers()
+        headers.append("Content-Type", "application/json")
+        const options: RequestInit = {
+            method: "POST",
+            headers,
+            mode: "cors",
+            body: JSON.stringify(order),
+        }
+        await fetch(submitUrl, options)
+                .then((response) => {
+                    if (response.ok) {
+                        return;
+                    }
+                    throw Error(
+                        "This request failed and no data was sent!"
+                    );
+                })
+                .then(() =>{
+                    setIsLoading(false);
+                    setError(null);
+                    //navigate("/finish");
+                })
+                .catch((er) => {
+                    setIsLoading(false);
+                    setError(er);
+                });
+    };
+
+    const onChangeMarketing = () => {
+        setMarketing(!marketing);
+    };
+
+    const changeTextArea = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setComment(event.target.value);
+    };
+
+    return (
+        <div className="terms-box">
+            <h2 className="address-row">Indsendelse af order</h2>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="submitbox">
+                    <p className="submitinfo">
+                        Inden at du kan indsende din order, SKAL du acceptere handelsbetingelserne for denne webshop. 
+                        Du kan finde mere information om siden handelsbetingelser <a href="">her</a>. 
+                    </p>
+                    <p className="checkbox-paragraf">
+                        <input
+                            required
+                            type="checkbox"
+                            id="checkbox-terms"
+                            name="checkbox-terms"
+                        />
+                        <label htmlFor="checkbox-terms" id="checkbox-label">
+                            Jeg accepterer vilkårene og betingelserne og privatlivsaftalen.
+                        </label>
+                    </p>
+                    <p className="checkbox-paragraf">
+                        <input
+                            type="checkbox"
+                            id="checkmarketing"
+                            name="checkmarketing"
+                            checked={marketing}
+                            onChange={onChangeMarketing}
+                        />
+                        <label htmlFor="checkmarketing" id="checkbox-label">
+                            Jeg accepterer at modtage marketingmails fra denne webshop.
+                        </label>
+                    </p>
+                    <p>
+                        <label htmlFor="submitcomment" id="submit-label">
+                            Tilføj en yderligere kommentar
+                        </label>
+                        <textarea 
+                            rows={4}
+                            id="submitcomment"
+                            className="submitcomment"
+                            name="submitcomment"
+                            placeholder={"Indtast kommentar her..."}
+                            value={comment}
+                            onChange={changeTextArea}
+                        />
+                    </p>
+                </div>
+                <button className="payment-btn" type="submit">
+                    Indsend order
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Submit;
